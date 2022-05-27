2b5aee73646c942cd9ca0032672ca47ecbbceb61
B1rtek <b1rtek@fakemail.com>
2022-05-28 00:52:28 +0200

    Changed the algorithm of the TexasBot to plan moves and execute them at planned time

--- GamblerLib/TestTexasBot.cpp
+++ GamblerLib/TestTexasBot.cpp
@@ -8,21 +8,27 @@ TestTexasBot::TestTexasBot(int balance, Game *game, const std::string &name) : G
 
 void TestTexasBot::makeAMove(int millisecondsPassed) noexcept {
     if (this->gamePlayed != nullptr) {
-        if(dynamic_cast<TexasHoldem*>(this->gamePlayed)->getCurrentPlayer() != this) return;
-        switch (millisecondsPassed % 3) {
-            case 0: {
-                if(millisecondsPassed % 6 == 0) {
-                    dynamic_cast<TexasHoldem*>(this->gamePlayed)->fold(this);
+        if (dynamic_cast<TexasHoldem *>(this->gamePlayed)->getCurrentPlayer() != this) return;
+        if (!this->moveScheduled) {
+            this->targetTime = millisecondsPassed + 300;
+            this->moveScheduled = true;
+        } else if (this->targetTime <= millisecondsPassed) {
+            switch (millisecondsPassed % 3) {
+                case 0: {
+                    if (millisecondsPassed % 7 == 0) {
+                        dynamic_cast<TexasHoldem *>(this->gamePlayed)->fold(this);
+                    }
+                }
+                    break;
+                case 1: {
+                    dynamic_cast<TexasHoldem *>(this->gamePlayed)->call(this);
+                }
+                    break;
+                case 2: {
+                    dynamic_cast<TexasHoldem *>(this->gamePlayed)->raise(this, 5);
                 }
             }
-                break;
-            case 1: {
-                dynamic_cast<TexasHoldem*>(this->gamePlayed)->call(this);
-            }
-                break;
-            case 2: {
-                dynamic_cast<TexasHoldem*>(this->gamePlayed)->raise(this, 2);
-            }
+            this->moveScheduled = false;
         }
     }
 }
