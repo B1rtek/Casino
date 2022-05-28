58e7c197e3092e0f28d957a9f72d63ca83af1d3f
B1rtek <b1rtek@fakemail.com>
2022-05-29 01:20:36 +0200

    Partially implemented the Roulette game

--- GameLib/Roulette.cpp
+++ GameLib/Roulette.cpp
@@ -1,8 +1,92 @@
 #include "Roulette.h"
 
-Roulette::Roulette(int minimumEntry, const std::string &name) : Game(minimumEntry, name) {}
+#include <random>
 
-Roulette::Roulette(Gambler *gambler, int minimumEntry, const std::string &name) : Game(gambler, minimumEntry, name) {}
+Roulette::Roulette(int minimumEntry, const std::string &name) noexcept: Game(minimumEntry, name) {}
 
-Roulette::Roulette(const std::vector<Gambler *> &gamblers, int minimumEntry, const std::string &name) :
+Roulette::Roulette(Gambler *gambler, int minimumEntry, const std::string &name) noexcept: Game(gambler, minimumEntry,
+                                                                                               name) {}
+
+Roulette::Roulette(const std::vector<Gambler *> &gamblers, int minimumEntry, const std::string &name) noexcept:
         Game(gamblers, minimumEntry, name) {}
+
+void Roulette::advanceGame(int millisecondsPassed) {
+    if (this->inProgress) {
+        if (this->targetTime <= millisecondsPassed) { // time for betting ended
+            this->rollTheNumber();
+            this->checkAndPayBets();
+            this->removeBankruptPlayers();
+            this->targetTime = millisecondsPassed + 30000; // new game begins in 30 seconds
+        } else {
+            for (auto &gambler: this->gamblersPlaying) {
+                if (gambler->isBot()) {
+                    gambler->makeAMove(millisecondsPassed);
+                }
+            }
+        }
+    } else {
+        if (this->targetTime <= millisecondsPassed) {
+            this->startGame();
+            this->targetTime = millisecondsPassed + 60000; // give everyone 60 seconds for betting
+        }
+    }
+}
+
+void Roulette::rollTheNumber() noexcept {
+    std::mt19937 mt(time(nullptr));
+    this->lastNumberRolled = mt() % 37;
+}
+
+void Roulette::checkAndPayBets() noexcept {
+    for (auto &bet: this->bets) {
+        switch (bet.type) {
+            case COLOR:
+            case EVENODD: {
+                if (this->lastNumberRolled % 2 == bet.number % 2 && this->lastNumberRolled != 0) {
+                    this->inGameMoney[bet.gambler] += 2 * bet.amount;
+                    bet.successful = true;
+                }
+            }
+                break;
+            case HALF: {
+                if ((this->lastNumberRolled >= 1 && this->lastNumberRolled <= 18 && bet.number == 1) ||
+                    (this->lastNumberRolled > 18 && bet.number > 1)) {
+                    this->inGameMoney[bet.gambler] += 2 * bet.amount;
+                    bet.successful = true;
+                }
+            }
+                break;
+            case TWELVE: {
+                if ((this->lastNumberRolled >= 1 && this->lastNumberRolled <= 12 && bet.number == 1) ||
+                    (this->lastNumberRolled >= 13 && this->lastNumberRolled <= 24 && bet.number == 13) ||
+                    (this->lastNumberRolled >= 25 && bet.number == 25)) {
+                    this->inGameMoney[bet.gambler] += 3 * bet.amount;
+                    bet.successful = true;
+                }
+            }
+                break;
+            case COLUMN: {
+                if (this->lastNumberRolled % 3 == bet.number % 3 && this->lastNumberRolled != 0) {
+                    this->inGameMoney[bet.gambler] += 3 * bet.amount;
+                    bet.successful = true;
+                }
+            }
+                break;
+            case NUMBER: {
+                if (this->lastNumberRolled == bet.number) {
+                    this->inGameMoney[bet.gambler] += 36 * bet.amount;
+                    bet.successful = true;
+                } else if (this->lastNumberRolled == 0) {
+                    this->inGameMoney[bet.gambler] += bet.amount / 2;
+                }
+            }
+                break;
+        }
+    }
+}
+
+void Roulette::startGame() noexcept {
+    Game::startGame();
+    this->bets.clear();
+}
+
--- GameLib/Roulette.h
+++ GameLib/Roulette.h
@@ -3,13 +3,81 @@
 
 #include "Game.h"
 
+enum RouletteBetType {
+    COLOR,
+    EVENODD,
+    HALF,
+    TWELVE,
+    COLUMN,
+    NUMBER
+};
+
+struct RouletteBet {
+    RouletteBet(RouletteBetType type, int number, int amount) : type(type), number(number), amount(amount) {};
+
+    RouletteBetType type;
+    Gambler *gambler;
+    int number, amount;
+    bool successful = false;
+
+    std::string getBetStringRepresentation() {
+        std::string representation = this->gambler->getName() + ": ";
+        switch (type) {
+            case COLOR: {
+                representation = number == 1 ? "Black, " : "Red, ";
+            }
+                break;
+            case EVENODD: {
+                representation = number == 1 ? "Odd, " : "Even, ";
+            }
+                break;
+            case HALF: {
+                representation = number == 1 ? "1 to 18, " : "19 to 36, ";
+            }
+                break;
+            case TWELVE: {
+                if (number == 1) representation = "1st 12, ";
+                if (number == 13) representation = "2nd 12, ";
+                if (number == 25) representation = "3rd 12, ";
+            }
+                break;
+            case COLUMN: {
+                if (number == 1) representation = "Column 1, ";
+                if (number == 2) representation = "Column 2, ";
+                if (number == 3) representation = "Column 3, ";
+            }
+                break;
+            case NUMBER: {
+                representation = "Number " + std::to_string(number) + ", ";
+            }
+                break;
+        }
+        representation += std::to_string(amount);
+        return representation;
+    }
+};
+
 class Roulette : public Game {
+    int lastNumberRolled = 0;
+    std::vector<RouletteBet> bets;
+
+    void rollTheNumber() noexcept;
+
+    void checkAndPayBets() noexcept;
+
 public:
-    explicit Roulette(int minimumEntry, const std::string &name = "");
+    void startGame() noexcept override;
 
-    Roulette(Gambler *gambler, int minimumEntry, const std::string &name = "");
+public:
+    explicit Roulette(int minimumEntry, const std::string &name = "") noexcept;
 
-    Roulette(const std::vector<Gambler *> &gamblers, int minimumEntry, const std::string &name = "");
+    Roulette(Gambler *gambler, int minimumEntry, const std::string &name = "") noexcept;
+
+    Roulette(const std::vector<Gambler *> &gamblers, int minimumEntry, const std::string &name = "") noexcept;
+
+    void advanceGame(int millisecondsPassed) override;
+
+
 };
 
 
